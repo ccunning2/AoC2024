708c2c837cf3f8261f36fd5aed9fad2f1caa02e0
Cameron Cunning <negatifzeo@gmail.com>
2024-12-05 20:46:48 -0600

    Cleanup

--- 2/day2.pl
+++ 2/day2.pl
@@ -28,9 +28,6 @@ convert_to_numbers([H|T], Acc, Result) :-
     append(Acc, [Num], NewAcc),
     convert_to_numbers(T, NewAcc, Result).
 
-
-
-
 check_safety(List) :-
     is_sequential(List),
     no_sequential_diff_gt_three(List).   
@@ -55,15 +52,6 @@ is_sequential([A, B | Rest]) :-
     is_decreasing([B | Rest]).
 
 
-is_sequential_2([]).
-is_sequential_2([_]).
-is_sequential_2([A, B | Rest]) :-
-    A < B, 
-    is_increasing_2([B | Rest]).
-is_sequential_2([A, B | Rest]) :-
-    A > B, 
-    is_decreasing_2([B | Rest]).
-
 
 is_increasing([]).
 is_increasing([_]).
@@ -72,29 +60,11 @@ is_increasing([A, B | Rest]) :-
     is_increasing([B | Rest]).
 
 
-is_increasing_2([]).
-is_increasing_2([_]).
-is_increasing_2([A, B | Rest]) :-
-    (
-        A < B -> is_increasing_2([B | Rest]);
-        is_increasing([B|Rest])
-    ).
-
-
 is_decreasing([]).
 is_decreasing([_]).
 is_decreasing([A, B | Rest]) :-
     A > B,
     is_decreasing([B | Rest]).
-
-
-is_decreasing_2([]).
-is_decreasing_2([_]).
-is_decreasing_2([A, B | Rest]) :-
-    (
-    A > B -> is_decreasing_2([B | Rest]);
-    is_decreasing([B | Rest])
-    ).
 
 
 no_sequential_diff_gt_three([]).
@@ -105,16 +75,6 @@ no_sequential_diff_gt_three([A,B | Rest]) :-
     no_sequential_diff_gt_three([B | Rest]).
 
 
-
-no_sequential_diff_gt_three_2([]).
-no_sequential_diff_gt_three_2([_]).
-no_sequential_diff_gt_three_2([A,B | Rest]) :-
-    Diff is abs(A-B),
-    Diff < 4 -> no_sequential_diff_gt_three_2([B | Rest]);
-    no_sequential_diff_gt_three([A | Rest]). %Remove B
-
-
-
 count_safe([], Acc, Acc).
 count_safe([H | T], Acc, Total) :-
     ( check_safety(H) -> (NewAcc is Acc + 1)
@@ -122,7 +82,6 @@ count_safe([H | T], Acc, Total) :-
     (NewAcc is Acc)
     ),
     count_safe(T, NewAcc, Total).
-
 
 
 count_safe_2([], Acc, Acc).
@@ -140,33 +99,13 @@ count_safe_2([H | T], Acc, Total) :-
     NewAcc is Acc,
     count_safe_2(T, NewAcc, Total)
     ).
-% count_safe_2([H | T], Acc, Total) :-
-%     (   
-%         % Case 1: H passes check_safety_2
-%         check_safety_2(H) -> 
-%         NewAcc is Acc + 1, asserta(good(H)),
-%         count_safe_2(T, NewAcc, Total)
-%     ;
-%         % Case 2: H is a list and its tail passes check_safety_2
-%         (is_list(H), H = [_ | Tail], check_safety(Tail) ->
-%         NewAcc is Acc + 1, asserta(tail(Tail)),
-%         count_safe_2(T, NewAcc, Total)
-%         ;
-%         % Case 3: Neither passes; do not increment
-%         NewAcc is Acc, asserta(bad(H)),
-%         count_safe_2(T, NewAcc, Total)
-%         )
-%     ).
-% 622, 746
 
-remove_one_item_check(List, Condition) :-
+remove_one_item_check(List, Condition) :-  %Here 'condition' can be any rule/1 (that takes a List)
     remove_one(List, NewList),
     call(Condition, NewList),
     !. %Add the 'cut' here to stop more searches
 
 
-%Example that should pass [37,38,37,34,31]
-%Another [73, 71, 69, 68, 65, 66, 62] -- By removing 66
 remove_one([_], []).
 remove_one([H|T], T).
 remove_one([H|T], [H|Modified]) :-
